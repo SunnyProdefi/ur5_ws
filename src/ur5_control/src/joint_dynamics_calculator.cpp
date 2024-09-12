a79f3a6b446f07d7e542c1908eac55553b4711c3
sunnyprodefi <sunprodefi@gmail.com>
2024-09-12 09:50:11 +0800

    Complete CTC

--- src/ur5_control/src/joint_dynamics_calculator.cpp
+++ src/ur5_control/src/joint_dynamics_calculator.cpp
@@ -21,10 +21,15 @@ vector<vector<double>> tau;
 
 vector<double> joint_positions;
 vector<double> joint_velocities;
-vector<double> joint_accelerations;
+vector<double> joint_tau;
 
 ros::Publisher joint_effort_pub;
 bool received_state = false;
+
+// 从 joint_states 到 Pinocchio 的映射
+vector<int> ros_to_pinocchio_map = {2, 1, 0, 3, 4, 5};
+// 从 Pinocchio 到 joint_states 的映射
+vector<int> pinocchio_to_ros_map = {2, 1, 0, 3, 4, 5};
 
 void initializeModel(const string &urdf_path) {
   pinocchio::urdf::buildModel(urdf_path, model);
@@ -52,33 +57,80 @@ void computeDynamics(const pinocchio::Model &model, pinocchio::Data &data,
   cout << "Torques: " << calcTau.transpose() << endl;
   vector<double> tau_i =
       vector<double>(calcTau.data(), calcTau.data() + calcTau.size());
-  double temp = tau_i[0];
-  tau_i[0] = tau_i[2];
-  tau_i[2] = temp;
   tau.push_back(tau_i);
-  //   pinocchio::crba(model, data, q);
-  //   Eigen::MatrixXd &M = data.M;
-  //   cout << "Mass matrix:\n" << M << endl;
-  //   Eigen::VectorXd c = pinocchio::nonLinearEffects(model, data, q, v);
-  //   cout << "Coriolis and centrifugal forces: " << c.transpose() << endl;
-  //   Eigen::VectorXd g = pinocchio::computeGeneralizedGravity(model, data, q);
-  //   cout << "Gravity vector: " << g.transpose() << endl;
 }
 
 void jointStateCallback(const sensor_msgs::JointState::ConstPtr &msg) {
   joint_positions.resize(msg->position.size());
-  for (size_t i = 0; i < msg->position.size(); ++i) {
-    joint_positions[i] = msg->position[i];
-  }
   joint_velocities.resize(msg->velocity.size());
-  for (size_t i = 0; i < msg->velocity.size(); ++i) {
-    joint_velocities[i] = msg->velocity[i];
+  joint_tau.resize(msg->effort.size());
+
+  // 重排关节位置、速度和加速度
+  // shoulder_pan_joint shoulder_lift_joint elbow_joint
+  // wrist_1_joint wrist_2_joint wrist_3_joint
+  for (size_t i = 0; i < 6; ++i) {
+    joint_positions[ros_to_pinocchio_map[i]] = msg->position[i];
+    joint_velocities[ros_to_pinocchio_map[i]] = msg->velocity[i];
+    joint_tau[ros_to_pinocchio_map[i]] = msg->effort[i];
   }
-  joint_accelerations.resize(msg->effort.size());
-  for (size_t i = 0; i < msg->effort.size(); ++i) {
-    joint_accelerations[i] = msg->effort[i];
-  }
+
   received_state = true;
+}
+
+void printVector(const string &name, const vector<double> &vec) {
+  cout << name << ": ";
+  for (size_t i = 0; i < vec.size(); ++i) {
+    cout << vec[i] << " ";
+  }
+  cout << endl;
+}
+
+void printJointData(const vector<vector<double>> &q_vecs,
+                    const vector<vector<double>> &v_vecs,
+                    const vector<double> &joint_positions,
+                    const vector<double> &joint_velocities, int i) {
+  cout << "q_des: ";
+  for (int j = 0; j < 6; ++j) {
+    cout << q_vecs[j][i] << " ";
+  }
+  cout << endl;
+
+  printVector("joint_positions", joint_positions);
+
+  cout << "v_des: ";
+  for (int j = 0; j < 6; ++j) {
+    cout << v_vecs[j][i] << " ";
+  }
+  cout << endl;
+
+  printVector("joint_velocities", joint_velocities);
+}
+
+// 封装读取 CSV 文件的函数
+void readTrajectoryFromFile(const string &filename, vector<double> &q_vec,
+                            vector<double> &v_vec, vector<double> &a_vec,
+                            int num) {
+  ifstream file(filename);
+  string line, cell;
+
+  if (file.is_open()) {
+    getline(file, line);  // 读取并跳过标题行
+    int i = 0;
+    while (getline(file, line) && i < num) {
+      stringstream lineStream(line);
+      getline(lineStream, cell, ',');  // 跳过时间列
+      getline(lineStream, cell, ',');
+      q_vec[i] = stod(cell);
+      getline(lineStream, cell, ',');
+      v_vec[i] = stod(cell);
+      getline(lineStream, cell, ',');
+      a_vec[i] = stod(cell);
+      i++;
+    }
+    file.close();
+  } else {
+    cerr << "Failed to open file: " << filename << endl;
+  }
 }
 
 int main(int argc, char **argv) {
@@ -105,35 +157,18 @@ int main(int argc, char **argv) {
       "/home/prodefi/ur5_ws/wrist_1_joint_trajectory.csv",
       "/home/prodefi/ur5_ws/wrist_2_joint_trajectory.csv",
       "/home/prodefi/ur5_ws/wrist_3_joint_trajectory.csv"};
+
   int hz = 50;
   int time = 3;
   int num = hz * time;
   vector<vector<double>> q_vecs(6, vector<double>(num)),
       v_vecs(6, vector<double>(num)), a_vecs(6, vector<double>(num));
 
+  // 调用封装的函数读取每个文件
   for (int f = 0; f < files.size(); ++f) {
-    ifstream file(files[f]);
-    string line, cell;
-
-    if (file.is_open()) {
-      getline(file, line);  // 读取并跳过标题行
-      int i = 0;
-      while (getline(file, line) && i < num) {
-        stringstream lineStream(line);
-        getline(lineStream, cell, ',');  // 跳过时间列
-        getline(lineStream, cell, ',');
-        q_vecs[f][i] = stod(cell);
-        getline(lineStream, cell, ',');
-        v_vecs[f][i] = stod(cell);
-        getline(lineStream, cell, ',');
-        a_vecs[f][i] = stod(cell);
-        i++;
-      }
-      file.close();
-    } else {
-      cerr << "Failed to open file: " << files[f] << endl;
-    }
+    readTrajectoryFromFile(files[f], q_vecs[f], v_vecs[f], a_vecs[f], num);
   }
+
   // 打印q_des,v_des,a_des
   cout << "q_des: " << q_des.transpose() << endl;
   cout << "v_des: " << v_des.transpose() << endl;
@@ -151,7 +186,6 @@ int main(int argc, char **argv) {
     cout << "v_des: " << v_des.transpose() << endl;
     cout << "a_des: " << a_des.transpose() << endl;
     pinocchio::forwardKinematics(model, data, q_des, v_des, a_des);
-    // printModelDataDetails(model, data);
     computeDynamics(model, data, q_des, v_des, a_des);
   }
   ros::Rate rate(50);
@@ -161,25 +195,42 @@ int main(int argc, char **argv) {
     cout << "Waiting for joint states..." << endl;
   }
 
-  for (int i = 0; i < num; i++) {
-    for (int j = 0; j < 2; ++j) {
-      if (j == 0) tau[i][j] = tau[i][j] / 1.2;
-      if (j == 1) tau[i][j] = tau[i][j] / 2.6;
-    }
-  }
-
   int i = 0;
   vector<double> Kp(6);
   vector<double> Kd(6);
+  // shoulder_pan_joint
   // elbow_joint
   // shoulder_lift_joint
-  // shoulder_pan_joint
   // wrist_1_joint
   // wrist_2_joint
   // wrist_3_joint
-  Kp = {10, 10, 0.1, 0.1, 0.1, 0.1};
-  Kd = {0.1, 0.1, 0.01, 0.01, 0.01, 0.01};
+  Kp = {0.1, 5, 5, 0.1, 0.1, 0.1};
+  Kd = {0.01, 1, 1, 0.01, 0.01, 0.01};
   vector<double> position_error(6), velocity_error(6);
+
+  // 插值到tau_ff的起始点,避免第一次下发的力矩突变,导致机械臂抖动,插值时间为0.5s
+  vector<double> tau_ff_start = joint_tau;
+  vector<double> tau_ff_end = tau[0];
+  vector<double> tau_ff(6);
+  int k = 0;
+  while (ros::ok() && k < 500) {
+    for (int j = 0; j < 6; ++j) {
+      tau_ff[j] = tau_ff_start[j] + (tau_ff_end[j] - tau_ff_start[j]) * k / 500;
+    }
+    k++;
+    std_msgs::Float64MultiArray effort_msg;
+    cout << "tau_ff: ";
+    effort_msg.data.clear();  // 确保每次下发时都是清空状态
+    for (int j = 0; j < tau_ff.size(); ++j) {
+      effort_msg.data.push_back(tau_ff[j]);
+      cout << effort_msg.data[j] << " ";
+    }
+    cout << endl;
+    joint_effort_pub.publish(effort_msg);
+    ros::spinOnce();
+    rate.sleep();
+  }
+
   while (ros::ok()) {
     std_msgs::Float64MultiArray effort_msg;
     cout << "----------------------i----------------------: " << i << endl;
@@ -187,58 +238,35 @@ int main(int argc, char **argv) {
     if (i > 149) i = 149;
     // 计算反馈力矩 tau_fb = Kp * (q_des - q) + Kd * (v_des - v)
     // 打印q_des、joint_positions、v_des、joint_velocities
-    cout << "q_des: " << q_vecs[2][i] << " " << q_vecs[1][i] << " "
-         << q_vecs[0][i] << " " << q_vecs[3][i] << " " << q_vecs[4][i] << " "
-         << q_vecs[5][i] << endl;
-    cout << "joint_positions: ";
+    printJointData(q_vecs, v_vecs, joint_positions, joint_velocities, i);
+    printVector("joint_tau", joint_tau);
     for (int j = 0; j < 6; ++j) {
-      cout << joint_positions[j] << " ";
-    }
-    cout << "\nv_des: " << v_vecs[2][i] << " " << v_vecs[1][i] << " "
-         << v_vecs[0][i] << " " << v_vecs[3][i] << " " << v_vecs[4][i] << " "
-         << v_vecs[5][i] << endl;
-    cout << "joint_velocities: ";
-    for (int j = 0; j < 6; ++j) {
-      cout << joint_velocities[j] << " ";
-    }
-
-    position_error[0] = q_vecs[2][i] - joint_positions[0];
-    velocity_error[0] = v_vecs[2][i] - joint_velocities[0];
-    position_error[2] = q_vecs[0][i] - joint_positions[2];
-    velocity_error[2] = v_vecs[0][i] - joint_velocities[2];
-    for (int j = 0; j < 6; ++j) {
-      if (j == 0 || j == 2) continue;
       position_error[j] = q_vecs[j][i] - joint_positions[j];
       velocity_error[j] = v_vecs[j][i] - joint_velocities[j];
     }
     // 打印position_error、velocity_error
-    cout << "\nposition_error: ";
-    for (int j = 0; j < 6; ++j) {
-      cout << position_error[j] << " ";
-    }
-    cout << "\nvelocity_error: ";
-    for (int j = 0; j < 6; ++j) {
-      cout << velocity_error[j] << " ";
-    }
+    printVector("position_error", position_error);
+    printVector("velocity_error", velocity_error);
+
+    // 计算反馈力矩
     vector<double> tau_fb(6);
-    cout << "\ntau_fb: ";
     for (int j = 0; j < 6; ++j) {
       tau_fb[j] = Kp[j] * position_error[j] + Kd[j] * velocity_error[j];
-      cout << tau_fb[j] << " ";
     }
-    cout << "\ntau_ff: " << "i: " << i << " ";
-    for (int j = 0; j < 6; ++j) {
-      cout << tau[i][j] << " ";
-    }
-    cout << "\ntau: ";
+    printVector("tau_fb", tau_fb);
+    printVector("tau_ff", tau[i]);
+
+    // 使用映射表将 tau 重新排序
+    cout << "tau: ";
+    effort_msg.data.clear();  // 确保每次下发时都是清空状态
     for (int j = 0; j < tau[i].size(); ++j) {
       effort_msg.data.push_back(tau[i][j] + tau_fb[j]);
-      //   effort_msg.data.push_back(tau[i][j]);
       cout << effort_msg.data[j] << " ";
     }
     cout << endl;
     i++;
     joint_effort_pub.publish(effort_msg);
+
     ros::spinOnce();
     rate.sleep();
   }
